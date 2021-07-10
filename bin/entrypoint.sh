8b71e99d38e551a216ace2aef1a4fa3583333bcb
thiago-scherrer <eu@thiago.info>
2021-07-10 11:11:34 -0300

    wip

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 function tfEnv () {
-	# remove me
-	ls -hal
 	tfenv install \
 	&& tfenv use
 }
 
 function terraForm () {
-	# remove me
-	ls -hal
-	echo $1
-	terraform $1
+	terraform $INPUT_TFARG
 }
 
 tfEnv
-terraForm $1
+terraForm
